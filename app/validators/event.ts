365e8c2fbf52755d55679f3e1ef0e863eba55d43
Giridhar Talla <giridhar.talla2002@gmail.com>
2024-05-20 22:56:24 +0530

    fix edit participant

--- app/validators/event.ts
+++ app/validators/event.ts
@@ -34,12 +34,13 @@ export const EditParticipantsValidator = vine.compile(
   vine.object({
     event_code: vine.string().minLength(5).maxLength(5),
     edit_code: vine.string().minLength(5).maxLength(5),
-    remove: vine.boolean(),
+    remove: vine.boolean().optional(),
     name: vine.string().maxLength(100).optional(),
     gender: vine.enum(['Male', 'Female']).optional(),
     address: vine.string().maxLength(50).optional(),
     can_pickup: vine.boolean().optional(),
     seats_available: vine.number().withoutDecimals().min(0).max(7).optional(),
+    confirmed: vine.boolean().optional(),
     phone_num: vine.string().maxLength(10).optional(),
   })
 )
